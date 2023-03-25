fd6777caa91830f496cb37a12dcc648bde635283
Martin Mwangi <mwangi.martin24@gmail.com>
2023-03-26 00:43:25 +0300

    Create jenkins file

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-  return "Welcome home!"
+  return "Welcome home again!"
 
 @app.route("/hello")
 def hello():
-  return "Hello there updated"
+  return "Hello there mart"
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=8080)
